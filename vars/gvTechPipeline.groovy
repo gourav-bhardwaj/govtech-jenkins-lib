991281b495bc68a800399050c1d8a5b4ea824c2c
Gourav kumar <47908345+gourav-bhardwaj@users.noreply.github.com>
2022-10-26 17:09:21 +0530

    Update gvTechPipeline.groovy

--- vars/gvTechPipeline.groovy
+++ vars/gvTechPipeline.groovy
@@ -5,6 +5,16 @@ def call(String name) {
     pipeline {
         agent any
         stages {
+            stage("GIT") {
+               steps {
+                   step([$class: 'WsCleanup'])
+                   //checkout scm
+                   sh 'mkdir -p helm-chart'
+                   dir('helm-chart') {
+                     git url: "https://github.com/gourav-bhardwaj/govtech-helm-chart-app.git", branch: 'dev', credentialsId: 'govtech-git-cred-id'
+                   }
+               }
+            }
             stage("Step - 1") {
                 steps {
                     echo "Welcome dear ${name}"
