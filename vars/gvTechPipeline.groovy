647f8167a0fc4e9e4ec4527761bc1d67c3e6f40d
Gourav kumar <47908345+gourav-bhardwaj@users.noreply.github.com>
2022-10-26 21:27:50 +0530

    Update gvTechPipeline.groovy

--- vars/gvTechPipeline.groovy
+++ vars/gvTechPipeline.groovy
@@ -21,32 +21,7 @@ def call(String name) {
             stage("Env Variable") {
                steps {
                  script {
-                   sh "git rev-parse --short HEAD > .git/commit"
-                   sh "basename `git rev-parse --show-toplevel` > .git/image"
-                   COMMIT = readFile('.git/commit').trim()
-                   echo "COMMIT ID is $COMMIT"
-                   sh 'git name-rev --name-only HEAD > GIT_BRANCH'
-                   sh 'cat GIT_BRANCH | cut -f3 -d "/" > test'
-                   BRANCH_NAME = readFile('test').trim()
-                   NAMESPACE = ""
-                   CHANNEL = ""
-                   KUBE_CONTEXT = ""
-                   KUBE_CREDENTIAL_ID = ""
-                   if (BRANCH_NAME == 'dev') {
-                        NAMESPACE = "gv-tech"
-                        CHANNEL = "dev"
-                        KUBE_CONTEXT = "kubernetes-admin@kubernetes"
-                        KUBE_CREDENTIAL_ID = "GOVTECH_KUBE_CRED"
-                        NEW_BRANCH_NAME = readFile('test').trim()
-                        echo "********This is $NEW_BRANCH_NAME**************"
-               } else if (BRANCH_NAME == 'pre-dev') {
-                        NAMESPACE = "gov-tech-pre-dev"
-                        CHANNEL = "pre-dev"
-                        KUBE_CONTEXT = "kubernetes-admin@kubernetes"
-                        KUBE_CREDENTIAL_ID = "GOVTECH_KUBE_CRED"
-                        NEW_BRANCH_NAME = readFile('test').trim()
-                        echo "********This is $NEW_BRANCH_NAME**************"
-               }
+                   sh "git version"
                  }
                }
             }
