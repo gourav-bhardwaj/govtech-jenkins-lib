b470c75f65f81507ecc89af506cb2b4207171e3c
Gourav kumar <47908345+gourav-bhardwaj@users.noreply.github.com>
2022-10-26 23:00:16 +0530

    Update gvTechPipeline.groovy

--- vars/gvTechPipeline.groovy
+++ vars/gvTechPipeline.groovy
@@ -13,7 +13,7 @@ def call(String name) {
                    step([$class: 'WsCleanup'])
                    git url: "https://github.com/gourav-bhardwaj/govtech-api-gateway.git", branch: 'dev', credentialsId: 'govtech-git-cred-id'
                    script {
-                   currentWork = (sh "pwd")
+                   currentWork = sh "pwd"
                    sh "git init $currentWork/govtech-api-gateway"
                    sh "git --version"
                    sh "git fetch --no-tags --force --progress -- https://github.com/gourav-bhardwaj/govtech-api-gateway.git +refs/heads/dev:refs/remotes/origin/dev"
