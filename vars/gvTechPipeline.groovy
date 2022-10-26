ab30a77078275fbe81945d52750de68126725019
Gourav kumar <47908345+gourav-bhardwaj@users.noreply.github.com>
2022-10-26 17:02:53 +0530

    Update gvTechPipeline.groovy

--- vars/gvTechPipeline.groovy
+++ vars/gvTechPipeline.groovy
@@ -2,14 +2,5 @@
 
 // vars/gvTechPipeline.groovy
 def call(String name) {
-    pipeline {
-        agent any
-        stages {
-            stage("Step - 1") {
-                steps {
-                    echo "Welcome dear ${name}"
-                }
-            }
-        }
-    }
+   echo "Welcome dear ${name}"
 }
