2b1e4deccce7b15361239b5f7c12db66dc860309
Gourav kumar <47908345+gourav-bhardwaj@users.noreply.github.com>
2022-10-31 02:10:36 +0530

    Update helmDeploy.groovy

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -1,14 +1,15 @@
 def helmDeployStep() {
-    String KUBE_CONTEXT = "kubernetes-admin@kubernetes"
-    String KUBE_CREDENTIAL_ID = "GOV_KUBE_CONFIG"
-    String HELM_FILENAME = "deploy-dev"
-    String jobName = application
-    String NAMESPACE = BRANCH_NAME
     String BUILD_TIMESTAMP = "10-29-2022"
     String version = "11"
     String BRANCH_NAME = "dev"
     String DOCKER_REGISTRY = "govkumardocker"
     String application = "govtech-api-gateway"
+    String KUBE_CONTEXT = "kubernetes-admin@kubernetes"
+    String KUBE_CREDENTIAL_ID = "GOV_KUBE_CONFIG"
+    String HELM_FILENAME = "deploy-dev"
+    String jobName = application
+    String NAMESPACE = BRANCH_NAME
+
      withCredentials([file(credentialsId: "${KUBE_CREDENTIAL_ID}", variable: 'KUBECONFIG_CONTENT')]) {
         sh "pwd"
         sh "ls -ltr"
