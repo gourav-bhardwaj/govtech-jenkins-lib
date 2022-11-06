cfbc0290be6f1595a336667414761522a68725da
Gourav kumar <47908345+gourav-bhardwaj@users.noreply.github.com>
2022-11-06 13:08:36 +0530

    Update helmDeploy.groovy

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -1,9 +1,8 @@
-def helmDeployStep() {
+def helmDeployStep(gcpProject) {
     String BUILD_TIMESTAMP = "10-29-2022"
     String version = "11"
     String BRANCH_NAME = "dev"
     String DOCKER_REGISTRY = "gcr.io"
-    String GCP_PROJECT_NAME = credentials('my-gcp-project')
     String application = "govtech-api-gateway"
     String KUBE_CONTEXT = "kubernetes-admin@kubernetes"
     String KUBE_CREDENTIAL_ID = "GOV_KUBE_CONFIG"
@@ -15,22 +14,21 @@ def helmDeployStep() {
         sh "pwd"
         sh "ls -ltr"
         sh "helm version --kubeconfig ${KUBECONFIG_CONTENT} --kube-context ${KUBE_CONTEXT}"
-        sh "helm upgrade --install --namespace ${NAMESPACE} ${jobName} helm-chart/spring-boot -f values/${HELM_FILENAME}.yaml --set image.repository=${DOCKER_REGISTRY}/${GCP_PROJECT_NAME}/${application},image.tag=${BUILD_TIMESTAMP}.${version}.${BRANCH_NAME} --kubeconfig ${KUBECONFIG_CONTENT} --kube-context ${KUBE_CONTEXT} --debug --atomic"
+        sh "helm upgrade --install --namespace ${NAMESPACE} ${jobName} helm-chart/spring-boot -f values/${HELM_FILENAME}.yaml --set image.repository=${DOCKER_REGISTRY}/${gcpProject}/${application},image.tag=${BUILD_TIMESTAMP}.${version}.${BRANCH_NAME} --kubeconfig ${KUBECONFIG_CONTENT} --kube-context ${KUBE_CONTEXT} --debug --atomic"
     }
 }
 
-def dockerBuildAndPush() {
+def dockerBuildAndPush(gcpProject) {
     String DOCKER_CREDENTIALS_ID = "GOV_DOCKER_CRED"
     String BUILD_TIMESTAMP = "10-29-2022"
     String version = "11"
     String BRANCH_NAME = "dev"
     String DOCKER_REGISTRY = "gcr.io"
-    String GCP_PROJECT_NAME = credentials('my-gcp-project')
     String application = "govtech-api-gateway"
     withCredentials([usernamePassword(credentialsId: "${DOCKER_CREDENTIALS_ID}", passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
         sh "docker login -u ${DOCKER_USER} -p ${DOCKER_PASS} https://index.docker.io/v1/"
-        sh "docker build -t ${DOCKER_REGISTRY}/${GCP_PROJECT_NAME}/${application}:${BUILD_TIMESTAMP}.${version}.${BRANCH_NAME} ."
-        sh "docker push ${DOCKER_REGISTRY}/${GCP_PROJECT_NAME}/${application}:${BUILD_TIMESTAMP}.${version}.${BRANCH_NAME}"
+        sh "docker build -t ${DOCKER_REGISTRY}/${gcpProject}/${application}:${BUILD_TIMESTAMP}.${version}.${BRANCH_NAME} ."
+        sh "docker push ${DOCKER_REGISTRY}/${gcpProject}/${application}:${BUILD_TIMESTAMP}.${version}.${BRANCH_NAME}"
     }
 }
 
@@ -39,6 +37,9 @@ def call() {
         agent any
         tools {
             gradle 'mygradle'
+        }
+        environment {
+           GCP_PROJECT_NAME = credentials('my-gcp-project')
         }
         stages {
             stage("GIT") {
@@ -64,14 +65,14 @@ def call() {
             stage("Docker build & push") {
               steps {
                 script {
-                  dockerBuildAndPush()
+                  dockerBuildAndPush(GCP_PROJECT_NAME)
                 }
               }
             }
             stage("Helm Deploy") {
                 steps {
                     script {
-                       helmDeployStep()
+                       helmDeployStep(GCP_PROJECT_NAME)
                     }
                 }
             }
